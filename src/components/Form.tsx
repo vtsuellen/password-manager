8ccfd0fce49e57e0ad4d407e44cb58a3de09fdb6
vtsuellen <vtsuellen@hotmail.com>
2023-06-06 13:44:04 -0300

    feat: Adicionar componente Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,27 @@
 function Form() {
-  return <h1>Gerenciador de senhas</h1>;
+  return (
+    <form>
+      <h1>Gerenciador de senhas</h1>
+      <label htmlFor="serviceName">
+        <input type="text" id="serviceName" />
+        Nome do serviço
+      </label>
+      <label htmlFor="login">
+        <input type="text" id="login" />
+        Login
+      </label>
+      <label htmlFor="password">
+        <input type="password" id="password" />
+        Senha
+      </label>
+      <label htmlFor="url">
+        <input type="text" id="url" />
+        URL
+      </label>
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </form>
+  );
 }
 
 export default Form;
